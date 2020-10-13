53bb37ff3f96ad929febddf2b0873a0b91876c7f
Ryan <ryan.w.boehm@gmail.com>
2020-10-13 15:47:35 -0500

    animation for broadband adoption graphic

--- js/broadband_numbers.js
+++ js/broadband_numbers.js
@@ -1,4 +1,5 @@
 jQuery(function($){
+  // initialize animations
   AOS.init();
 
   // broadband adoption data
@@ -6,13 +7,13 @@ jQuery(function($){
               {"year":"2005", "percent": 32},
               {"year":"2010", "percent": 62},
               {"year":"2015", "percent": 68},
-              {"year":"2019", "percent": 81}];
+              {"year":"2020", "percent": 81}];
 
   var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 960 - margin.left - margin.right,
+      width = 1140 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
-  var formatDate = d3.time.format("%Y");
+  var parseDate = d3.time.format("%Y").parse;
 
   var x = d3.time.scale()
       .range([0, width]);
@@ -20,13 +21,22 @@ jQuery(function($){
   var y = d3.scale.linear()
       .range([height, 0]);
 
+  var tickLabels = ["2000", "2005", "2010", "2015", "2019"];
+
   var xAxis = d3.svg.axis()
+      .ticks(5)
       .scale(x)
+      .tickFormat(function(d,i){ return tickLabels[i] })
       .orient("bottom");
 
   var yAxis = d3.svg.axis()
       .scale(y)
       .orient("left");
+
+  var initialarea = d3.svg.area()
+      .x(function(d) { return x(d.year); })
+      .y0(height)
+      .y1(height);
 
   var area = d3.svg.area()
       .x(function(d) { return x(d.year); })
@@ -38,6 +48,12 @@ jQuery(function($){
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+  // Take each row and put the date column through the parsedate form we've defined above.
+  data.forEach(function(d) {
+    d.year = parseDate(d.year);
+  });
 
   x.domain(d3.extent(data, function(d) { return d.year; }));
   //y.domain(d3.extent(data, function(d) { return d.percent; }));
@@ -59,6 +75,11 @@ jQuery(function($){
   svg.append("path")
       .datum(data)
       .attr("class", "area")
+      .attr("fill", "#E71B4F")
+      .attr("d", initialarea) // initial state (line at the bottom)
+      .transition()
+      .duration(2500)
+      .ease("bounce")
       .attr("d", area);
 
 });
