0032ca1bb63ba77a757945573f036a536904a312
Ryan <ryan.w.boehm@gmail.com>
2020-10-15 12:55:25 -0500

    basic set-up for infrastructure chart

--- js/broadband_numbers.js
+++ js/broadband_numbers.js
@@ -57,7 +57,7 @@ jQuery(function($){
                 {"year":"2020", "percent": 81}];
 
     // dimensions
-    margin = {top: 20, right: 20, bottom: 30, left: 50};
+    margin = {top: 20, right: 20, bottom: 60, left: 50};
     width = 1140 - margin.left - margin.right;
     height = 500 - margin.top - margin.bottom;
     viewBox = "0 0 1140 500";
@@ -114,7 +114,13 @@ jQuery(function($){
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(xAxis)
+      .append("text")
+        .attr("y", 40)
+        .attr("x", 540)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Year");
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
@@ -159,7 +165,7 @@ jQuery(function($){
             {"year":"2017", "customers": 66}];
 
     // dimensions
-    margin = {top: 20, right: 20, bottom: 30, left: 50};
+    margin = {top: 20, right: 20, bottom: 60, left: 50};
     width = 1140 - margin.left - margin.right;
     height = 500 - margin.top - margin.bottom;
     viewBox = "0 0 1140 500";
@@ -174,10 +180,19 @@ jQuery(function($){
 
     xAxis = d3.svg.axis()
         .scale(x)
+        .tickSize(10,0)
         .orient("bottom");
 
     yAxis = d3.svg.axis()
         .scale(y)
+        .tickFormat(function(d,i) {
+          if (d == 0) {
+            return d;
+          }
+          else {
+            return d + "M";
+          }
+        })
         .orient("left");
 
     initialArea = d3.svg.area()
@@ -208,7 +223,13 @@ jQuery(function($){
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(xAxis)
+      .append("text")
+        .attr("y", 40)
+        .attr("x", 540)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Year");
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
@@ -217,7 +238,121 @@ jQuery(function($){
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Percent");
+        .text("Number of Customers");
+    svg.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("fill", "#5AC9E7")
+        .attr("d", initialArea) // initial state (line at the bottom)
+        .transition()
+        .duration(1500)
+        .ease("linear")
+        .attr("d", area);
+  }
+
+
+  // infrastructure investment chart
+  function infraChart() {
+    var data, margin, width, height, viewBox, parseDate, x, y,
+        tickLabels, xAxis, yAxis, initialArea, area, svg;
+
+    // broadband adoption data
+    data = [{"year":"1999", "amount": 30},
+            {"year":"2000", "amount": 40},
+            {"year":"2001", "amount": 50},
+            {"year":"2002", "amount": 55.8},
+            {"year":"2003", "amount": 70},
+            {"year":"2004", "amount": 80},
+            {"year":"2005", "amount": 90},
+            {"year":"2006", "amount": 100},
+            {"year":"2007", "amount": 114.1},
+            {"year":"2008", "amount": 130},
+            {"year":"2009", "amount": 150},
+            {"year":"2010", "amount": 170},
+            {"year":"2011", "amount": 190},
+            {"year":"2012", "amount": 206.6},
+            {"year":"2013", "amount": 220},
+            {"year":"2014", "amount": 235},
+            {"year":"2015", "amount": 250},
+            {"year":"2016", "amount": 265},
+            {"year":"2017", "amount": 280},
+            {"year":"2018", "amount": 290}];
+
+    // dimensions
+    margin = {top: 20, right: 20, bottom: 60, left: 50};
+    width = 1140 - margin.left - margin.right;
+    height = 500 - margin.top - margin.bottom;
+    viewBox = "0 0 1140 500";
+
+    parseDate = d3.time.format("%Y").parse;
+
+    x = d3.time.scale()
+        .range([0, width]);
+
+    y = d3.scale.linear()
+        .range([height, 0]);
+
+    xAxis = d3.svg.axis()
+        .scale(x)
+        .tickSize(10,0)
+        .orient("bottom");
+
+    yAxis = d3.svg.axis()
+        .scale(y)
+        .tickFormat(function(d,i) {
+          if (d == 0) {
+            return d;
+          }
+          else {
+            return d + "M";
+          }
+        })
+        .orient("left");
+
+    initialArea = d3.svg.area()
+        .x(function(d) { return x(d.year); })
+        .y0(0)
+        .y1(height);
+
+    area = d3.svg.area()
+        .x(function(d) { return x(d.year); })
+        .y0(height)
+        .y1(function(d) { return y(d.customers); });
+
+    svg = d3.select(".customers-chart").append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", viewBox)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    // Take each row and put the date column through the parsedate form we've defined above.
+    data.forEach(function(d) {
+      d.year = parseDate(d.year);
+    });
+
+    x.domain(d3.extent(data, function(d) { return d.year; }));
+    y.domain([0,80]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("y", 40)
+        .attr("x", 540)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Year");
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Number of Customers");
     svg.append("path")
         .datum(data)
         .attr("class", "area")
